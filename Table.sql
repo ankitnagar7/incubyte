95219c6d466b5f62c7edd9f99b1f28bc7da04313
ankitnagar7 <83772384+ankitnagar7@users.noreply.github.com>
2021-05-06 20:09:20 +0530

    Hospital patients data management using SSIS

---
+++ Table.sql
@@ -0,0 +1,71 @@
+create table Table_Staging(
+Name VARCHAR(255) Primary Key not null,
+Cust_I VARCHAR(18) not null,
+Open_Dt VARCHAR (8) not null,
+Consul_Dt VARCHAR (8),
+VAC_ID CHAR(5),
+DR_Name CHAR (255),
+State CHAR(5),
+County CHAR(5),
+DOB VARCHAR (8),
+FLAG CHAR(1));
+
+create table Table_IND(
+Name VARCHAR(255) Primary Key not null,
+Cust_I VARCHAR(18) not null,
+Open_Dt VARCHAR (8) not null,
+Consul_Dt VARCHAR (8),
+VAC_ID CHAR(5),
+DR_Name CHAR (255),
+State CHAR(5),
+County CHAR(5),
+DOB VARCHAR (8),
+FLAG CHAR(1));
+
+create table Table_USA(
+Name VARCHAR(255) Primary Key not null,
+Cust_I VARCHAR(18) not null,
+Open_Dt VARCHAR (8) not null,
+Consul_Dt VARCHAR (8),
+VAC_ID CHAR(5),
+DR_Name CHAR (255),
+State CHAR(5),
+County CHAR(5),
+DOB VARCHAR (8),
+FLAG CHAR(1));
+
+create table Table_PHIL(
+Name VARCHAR(255) Primary Key not null,
+Cust_I VARCHAR(18) not null,
+Open_Dt VARCHAR (8) not null,
+Consul_Dt VARCHAR (8),
+VAC_ID CHAR(5),
+DR_Name CHAR (255),
+State CHAR(5),
+County CHAR(5),
+DOB VARCHAR (8),
+FLAG CHAR(1));
+
+create table Table_NYC(
+Name VARCHAR(255) Primary Key not null,
+Cust_I VARCHAR(18) not null,
+Open_Dt VARCHAR (8) not null,
+Consul_Dt VARCHAR (8),
+VAC_ID CHAR(5),
+DR_Name CHAR (255),
+State CHAR(5),
+County CHAR(5),
+DOB VARCHAR (8),
+FLAG CHAR(1));
+
+create table Table_AU(
+Name VARCHAR(255) Primary Key not null,
+Cust_I VARCHAR(18) not null,
+Open_Dt VARCHAR (8) not null,
+Consul_Dt VARCHAR (8),
+VAC_ID CHAR(5),
+DR_Name CHAR (255),
+State CHAR(5),
+County CHAR(5),
+DOB VARCHAR (8),
+FLAG CHAR(1));
